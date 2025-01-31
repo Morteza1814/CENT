b9ab0118ead98443e94b7444bc4419190cae36aa
Alireza Khadem <arkhadem@umich.edu>
2025-01-30 22:37:51 -0500

    figure scripts re-moved

--- generate_figures.sh
+++ generate_figures.sh
@@ -1,12 +1,12 @@
 #!/bash/bin
 
-python figure_11.py
-python figure_12a.py
-python figure_12b.py
-python figure_12c.py
-python figure_12d.py
-python figure_13a.py
-python figure_13b.py
-python figure_13c.py
-python figure_14a.py
-python figure_14c.py
+python figure_scripts/figure_11.py
+python figure_scripts/figure_12a.py
+python figure_scripts/figure_12b.py
+python figure_scripts/figure_12c.py
+python figure_scripts/figure_12d.py
+python figure_scripts/figure_13a.py
+python figure_scripts/figure_13b.py
+python figure_scripts/figure_13c.py
+python figure_scripts/figure_14a.py
+python figure_scripts/figure_14c.py
